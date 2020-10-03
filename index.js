464ece5ee9bf124c1dfd56a40701c84f373dd319
cat - lin <plummorgan@gmail.com>
2020-10-03 01:15:50 -0700

    added question array

--- index.js
+++ index.js
@@ -1,16 +1,153 @@
+//some global variables
+const inquirer = require('inquirer');
+
+
 // array of questions for user
-const questions = [
+const readmeQuestions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the project you would like to display?',
+            validate: (titleInput) => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Your project must have a title!');
+                    return false;
+                }
+            },
+        },
+        {
+            type: 'confirm',
+            name: 'confirmDescription',
+            message: 'Would you like to add a description of your project?',
+            default: true,
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a description about your project.',
+            when: ({ confirmDescription }) => {
+                if (confirmDescription) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Tell other developers how to run your code.',
+            validate: (installInput) => {
+                if (installInput) {
+                    return true;
+                } else {
+                    console.log('You must explain how to run your code!');
+                    return false;
+                }
+            },
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Explain to other devlopers how to use your application!',
+            validate: (usageInput) => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('You must explain how to use your application!');
+                    return false;
+                }
+            },
+        },
+        {
+            type: 'confirm',
+            name: 'confirmLicense',
+            message: 'Would you like to add a license?',
+            default: true,
 
-];
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Select which license you would like to use.',
+            choices: ['MIT', 'BSD-2-Clause', 'GPL-2.0'],
+            when: ({ confirmLicense }) => {
+                if (confirmLicense) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'How would you prefer other developers to contribute to your code?',
+            validate: (contributionInput) => {
+                if (contributionInput) {
+                    return true;
+                } else {
+                    console.log('You must let other developers know how they can help or not help!');
+                    return false;
+                }
+            },
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'How would you like the user to test your application?',
+            validate: (contributionInput) => {
+                if (contributionInput) {
+                    return true;
+                } else {
+                    console.log('You must tell fellow developers how to test your application!');
+                    return false;
+                }
+            },
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please submit your Github user name.',
+            validate: (githubInput) => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Nope! You gotta tell us your user name!');
+                    return false;
+                }
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please submit your email address.',
+            validate: (emailInput) => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Nope! You gotta tell us your email address!');
+                    return false;
+                }
+            },
+        }
+    ]);
+};
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
 
-// function to initialize program
-function init() {
 
-}
+// // function to write README file
+// function writeToFile(fileName, data) {
 
-// function call to initialize program
-init();
+// }
+
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,7 @@
+//generate the description here
+
+//generate the license here
+
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
